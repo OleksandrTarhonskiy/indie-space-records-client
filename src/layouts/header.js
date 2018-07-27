b3b4c1162cb5b7826851b13f5ce06a4e2b8141a8
alextargonski <targonsky97@gmail.com>
2018-07-27 11:25:28 +0300

    remove unsable

--- src/layouts/header.js
+++ src/layouts/header.js
@@ -2,7 +2,6 @@ import React      from 'react';
 import styled     from 'styled-components';
 import AppBar     from '@material-ui/core/AppBar';
 import Toolbar    from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon   from '@material-ui/icons/Menu';
 
