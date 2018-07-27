86b62b9c7fa029511950849ff77367c0482b26ba
alextargonski <targonsky97@gmail.com>
2018-07-27 13:58:02 +0300

    add logo

--- src/layouts/header.js
+++ src/layouts/header.js
@@ -1,12 +1,14 @@
-import React      from 'react';
-import styled     from 'styled-components';
-import AppBar     from '@material-ui/core/AppBar';
-import Toolbar    from '@material-ui/core/Toolbar';
+import React   from 'react';
+import styled  from 'styled-components';
+import AppBar  from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
 
-import MenuBar    from './menu';
+import MenuBar from './menu';
+import Logo    from './theme/logo.png';
 
 const Header = () => (
   <Header.MenuBar>
+    <Header.LogoWrapper />
     <Header.Toolbar>
       <Header.ToolbarItem>
         Menu
@@ -16,19 +18,28 @@ const Header = () => (
   </Header.MenuBar>
 );
 
+Header.LogoWrapper = styled.div`
+  background      : url(${Logo}) no-repeat;
+  background-size : 270px 90px;
+  height          : 100px;
+  width           : 40%;
+`;
+
 Header.MenuBar = styled(AppBar)`
-  display : flex;
-  widht   : 100%;
-  background: linear-gradient(135deg, #4923b2 2%,#284dd3 58%,#207cca 100%,#7db9e8 100%);
+  display         : flex;
+  height          : 10%;
+  flex-direction  : row !important;
+  background      : linear-gradient(135deg, #4923b2 2%,#284dd3 58%,#207cca 100%,#7db9e8 100%);
+  padding         : 0 25px 0 25px;
 `;
 
 Header.Toolbar = styled(Toolbar)`
-  flex-direction  : row;
   justify-content : flex-end;
+  width           : 60%;
 `;
 
 Header.ToolbarItem = styled.h2`
-  font-family: 'Pacifico', cursive;
+  font-family : 'Pacifico', cursive;
 `;
 
 export default Header;
