a733224485f1945e5b9ad02ffaa15ab27dfc29b4
alextargonski <targonsky97@gmail.com>
2018-07-27 11:20:49 +0300

    add font and some style for header

--- src/layouts/header.js
+++ src/layouts/header.js
@@ -1,21 +1,37 @@
 import React      from 'react';
+import styled     from 'styled-components';
 import AppBar     from '@material-ui/core/AppBar';
 import Toolbar    from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
+import MenuIcon   from '@material-ui/icons/Menu';
 
 const Header = () => (
-  <AppBar>
-    <Toolbar>
+  <Header.MenuBar>
+    <Header.Toolbar>
+      <Header.ToolbarItem>
+        Menu
+      </Header.ToolbarItem>
       <IconButton color="inherit" aria-label="Menu">
         <MenuIcon />
       </IconButton>
-      <Typography variant="title" color="inherit">
-        Menu
-      </Typography>
-    </Toolbar>
-  </AppBar>
+    </Header.Toolbar>
+  </Header.MenuBar>
 );
+
+Header.MenuBar = styled(AppBar)`
+  display : flex;
+  widht   : 100%;
+  background: linear-gradient(135deg, #4923b2 2%,#284dd3 58%,#207cca 100%,#7db9e8 100%);
+`;
+
+Header.Toolbar = styled(Toolbar)`
+  flex-direction  : row;
+  justify-content : flex-end;
+`;
+
+Header.ToolbarItem = styled.h2`
+  font-family: 'Pacifico', cursive;
+`;
 
 export default Header;
