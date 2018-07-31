325cfbad4a29345ca631d9a438ac745d9215b6b5
alextargonski <targonsky97@gmail.com>
2018-07-31 18:46:00 +0300

    add filters state

--- src/modules/landing-page/components/artist_filters_panel.js
+++ src/modules/landing-page/components/artist_filters_panel.js
@@ -1,12 +1,24 @@
 import React        from 'react';
+import * as R       from 'ramda';
+import PropTypes    from 'prop-types';
 import styled       from 'styled-components';
 import IconButton   from '@material-ui/core/IconButton';
 import Headset      from '@material-ui/icons/Headset';
 import LocationIcon from '@material-ui/icons/LocationOn';
 import MusicNote    from '@material-ui/icons/MusicNote';
 import Toolbar      from '@material-ui/core/Toolbar';
+import {
+  compose,
+  withStateHandlers,
+}                   from 'recompose';
 
-const ArtistFilters = () => (
+const ArtistFilters = ({
+  filters: {
+    genre,
+    location,
+  },
+  openFilters,
+}) => (
   <ArtistFilters.SubSectionWrapper>
     <ArtistFilters.Toolbar>
       <ArtistFilters.Heading>
@@ -16,19 +28,46 @@ const ArtistFilters = () => (
         <Headset />
       </ArtistFilters.IconButton >
       <p>listenings</p>
-      <ArtistFilters.IconButton  color="inherit">
+      <ArtistFilters.IconButton
+        color="inherit"
+        onClick={openFilters}
+        name="location"
+        active={location}
+      >
         <LocationIcon />
       </ArtistFilters.IconButton >
       <p>Location</p>
-      <ArtistFilters.IconButton  color="inherit">
+      <ArtistFilters.IconButton
+        color="inherit"
+        onClick={openFilters}
+        name="genre"
+        active={genre}
+      >
         <MusicNote />
-      </ArtistFilters.IconButton >
+      </ArtistFilters.IconButton>
       <p>Genre</p>
       <ArtistFilters.IconButton  color="inherit">
         #
       </ArtistFilters.IconButton >
       <p>Most popular tags</p>
     </ArtistFilters.Toolbar>
+    {/* TODO: replace to separate components, add close handlers, add hover for buttons*/}
+    {
+      genre ?
+      <ArtistFilters.GenreList>
+        <p>indie-rock, dream-pop, trip-hop, alternative</p>
+      </ArtistFilters.GenreList>
+      :
+      null
+    }
+    {
+      location ?
+      <ArtistFilters.LocationList>
+       <p>Ontario, Toronto, London</p>
+      </ArtistFilters.LocationList>
+      :
+      null
+    }
   </ArtistFilters.SubSectionWrapper>
 );
 
@@ -47,6 +86,37 @@ ArtistFilters.Toolbar = styled(Toolbar)`
 
 ArtistFilters.IconButton = styled(IconButton)`
   margin : 0 2% !important;
+  background : ${props => (props.active ? '#EAEDF5 !important' : 'transparent !important')};
 `;
 
-export default ArtistFilters;
+ArtistFilters.GenreList = styled.div`
+  position : relative;
+`;
+
+ArtistFilters.LocationList = styled.div`
+  position : relative;
+`;
+
+ArtistFilters.propTypes = {
+  filters     : PropTypes.object.isRequired,
+  openFilters : PropTypes.func.isRequired,
+};
+
+const withState = compose(
+  withStateHandlers(
+    ({
+      filters = {
+       location : false,
+       genre    : false,
+     }
+    }) => ({ filters }),
+    {
+      openFilters : state => ({ target }) => {
+        const filters = R.assoc(target.name, true, state.filters);
+        return ({ filters })
+      }
+    },
+  ),
+);
+
+export default withState(ArtistFilters);
