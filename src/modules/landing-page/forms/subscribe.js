39bf013367a4b719ed691de94ea18fd737386094
alextargonski <targonsky97@gmail.com>
2018-08-07 19:43:15 +0300

    add reusable button component to subscribe form

--- src/modules/landing-page/forms/subscribe.js
+++ src/modules/landing-page/forms/subscribe.js
@@ -1,24 +1,17 @@
-import React      from 'react';
-import Button     from '@material-ui/core/Button';
-import TextField  from '@material-ui/core/TextField';
-import styled     from 'styled-components';
+import React        from 'react';
+import TextField    from '@material-ui/core/TextField';
+import styled       from 'styled-components';
+
+import CustomButton from '../components/custom_button';
 
 const SubscribeForm = () => (
   <SubscribeForm.SubscribeForm>
     <SubscribeForm.EmailInput
       label="Email"
     />
-    <SubscribeForm.Button>
-      Subscribe
-    </SubscribeForm.Button>
+    <CustomButton text="Subscribe" />
   </SubscribeForm.SubscribeForm>
 );
-
-SubscribeForm.Button = styled(Button)`
-  background : linear-gradient(135deg, #4923b2 2%,#284dd3 58%,#207cca 100%,#7db9e8 100%);
-  color      : #fff !important;
-  margin-top : 1% !important;
-`;
 
 SubscribeForm.EmailInput = styled(TextField)`
   margin-right : 10px !important;
