de3ba6ea7b6460255f6567b157b780d98c5457c3
alextargonski <targonsky97@gmail.com>
2018-08-08 21:29:31 +0300

    add changing position of header on scroll, change head background

--- src/layouts/header.js
+++ src/layouts/header.js
@@ -1,15 +1,26 @@
-import React      from 'react';
-import styled     from 'styled-components';
-import breakpoint from 'styled-components-breakpoint';
-import AppBar     from '@material-ui/core/AppBar';
-import Toolbar    from '@material-ui/core/Toolbar';
+import React                 from 'react';
+import styled, { keyframes } from 'styled-components';
+import * as R                from 'ramda';
+import breakpoint            from 'styled-components-breakpoint';
+import AppBar                from '@material-ui/core/AppBar';
+import Toolbar               from '@material-ui/core/Toolbar';
+import {
+  compose,
+  withStateHandlers,
+  lifecycle
+}                            from 'recompose';
 
-import MenuBar    from './menu';
-import Logo       from './theme/logo.png';
-import Mobile     from './theme/mobile-logo.png';
+import MenuBar               from './menu';
+import Logo                  from './theme/logo.png';
+import Mobile                from './theme/mobile-logo.png';
 
-const Header = () => (
-  <Header.MenuBar>
+const Header = ({
+  onScroll,
+  style: {
+    position,
+  }
+}) => (
+  <Header.MenuBar position={position}>
     <Header.LogoWrapper />
     <Header.Toolbar>
       <Header.ToolbarItem>
@@ -20,13 +31,30 @@ const Header = () => (
   </Header.MenuBar>
 );
 
+const keyFrameExampleOne = keyframes`
+  0% {
+    opacity : 0.7;
+  }
+  50% {
+    opacity : 0.8;
+  }
+  75% {
+    opacity : 0.9;
+  }
+  100% {
+    opacity : 1;
+  }
+`
+
 Header.LogoWrapper = styled.div`
   && {
-    background      : url(${Mobile}) no-repeat;
-    background-size : 80px 80px;
-    height          : 100px;
-    width           : 40%;
-    margin-top      : 3px;
+    background         : url(${Mobile}) no-repeat;
+    background-size    : 80px 80px;
+    height             : 100px;
+    width              : 40%;
+    margin-top         : 3px;
+    animation-name     : ${keyFrameExampleOne};
+    animation-duration : 1s;
 
     ${breakpoint('md')`
       background      : url(${Logo}) no-repeat;
@@ -37,11 +65,13 @@ Header.LogoWrapper = styled.div`
 `;
 
 Header.MenuBar = styled(AppBar)`
-  display         : flex;
-  height          : 70px;
-  flex-direction  : row !important;
-  background      : linear-gradient(to right,#723af9,#46aafc);
-  padding         : 0 25px 0 25px;
+  display        : flex;
+  height         : 70px;
+  flex-direction : row !important;
+  background     : linear-gradient(to right,#723af9,#46aafc) !important;
+  box-shadow     : none !important;
+  padding        : 0 25px 0 25px;
+  position       : ${props => props.position};
 `;
 
 Header.Toolbar = styled(Toolbar)`
@@ -53,4 +83,35 @@ Header.ToolbarItem = styled.h2`
   font-family : 'Pacifico', cursive;
 `;
 
-export default Header;
+const withState = compose(
+  withStateHandlers(
+    ({
+      style = {
+        position : 'relative',
+      }
+    }) => ({ style }),
+    {
+      onScroll : state => () => {
+        let style = null;
+        if (window.scrollY > 650) {
+          style = R.assoc('position', 'fixed', state.position);
+          console.log(style)
+        } else {
+          style = R.assoc('position', 'relative', state.position);
+        }
+        return ({style})
+      },
+    },
+  ),
+  lifecycle({
+    componentDidMount() {
+      console.log('mount')
+      window.addEventListener('scroll', this.props.onScroll);
+    },
+    componentWillUnmount() {
+      return window.removeEventListener('scroll', this.props.onScroll);
+    },
+  })
+);
+
+export default withState(Header);
