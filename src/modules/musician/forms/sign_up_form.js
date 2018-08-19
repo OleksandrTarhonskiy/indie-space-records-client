aaef0a7cce9901169eeaf54b12dbbd5808780420
alextargonski <targonsky97@gmail.com>
2018-08-19 11:18:35 +0300

    add recompose and validator

--- src/modules/musician/forms/sign_up_form.js
+++ src/modules/musician/forms/sign_up_form.js
@@ -1,8 +1,23 @@
-import React     from 'react';
-import TextField from '@material-ui/core/TextField';
-import styled    from 'styled-components';
+import React         from 'react';
+import TextField     from '@material-ui/core/TextField';
+import styled        from 'styled-components';
+import * as R        from 'ramda';
+import {
+  compose,
+  withStateHandlers,
+}                    from 'recompose';
+import validator     from 'validator';
 
-const MusicianSignUpForm = () => (
+const MusicianSignUpForm = ({
+  form: {
+    name,
+    email,
+    password,
+    confirmPassword,
+  },
+  handleChange,
+  canSubmit,
+}) => (
   <div>
     <MusicianSignUpForm.Headline>
       Sign up
@@ -10,21 +25,40 @@ const MusicianSignUpForm = () => (
     <form>
       <TextField
         id="name"
+        name="name"
         label="Name"
         margin="normal"
+        value={name}
+        onChange={handleChange}
         fullWidth
       />
       <TextField
         id="email"
+        name="email"
         label="Email"
         margin="normal"
+        value={email}
+        onChange={handleChange}
         fullWidth
       />
       <TextField
         id="password"
+        name="password"
         label="Password"
         type="password"
         margin="normal"
+        value={password}
+        onChange={handleChange}
+        fullWidth
+      />
+      <TextField
+        id="confirmPassword"
+        name="confirmPassword"
+        label="Confirm Password"
+        type="password"
+        margin="normal"
+        value={confirmPassword}
+        onChange={handleChange}
         fullWidth
       />
     </form>
@@ -36,4 +70,35 @@ MusicianSignUpForm.Headline = styled.h1`
   color       : #374142;
 `;
 
-export default MusicianSignUpForm;
+const canSubmitForm = ({ name, email, password, confirmPassword }) => R.all(R.equals(true))([
+  !R.isEmpty(name),
+  validator.isEmail(email),
+  validator.isLength(password, { min: 8 }),
+  !R.isEmpty(confirmPassword),
+  R.equals(password, confirmPassword),
+]);
+
+const withRecompose = compose(
+  withStateHandlers(
+    ({
+      form      = {
+        name            : '',
+        email           : '',
+        password        : '',
+        confirmPassword : '',
+      },
+      canSubmit = false,
+    }) => ({ form, canSubmit }),
+    {
+      handleChange : state => ({ target }) => {
+        const form = R.assoc(target.name, target.value, state.form);
+        return ({
+          form,
+          canSubmit : canSubmitForm(form),
+        });
+      },
+    },
+  ),
+);
+
+export default withRecompose(MusicianSignUpForm);
