408bf527256b715184c550f63e48832596af6d85
alextargonski <targonsky97@gmail.com>
2018-08-23 12:47:14 +0300

    add info columns

---
+++ src/modules/musician/components/features.js
@@ -0,0 +1,75 @@
+import React      from 'react';
+import styled     from 'styled-components';
+import breakpoint from 'styled-components-breakpoint';
+import Typography from '@material-ui/core/Typography';
+
+const Features = () => (
+  <Features.FeaturesWrapper>
+    <Features.FeatureInfo>
+      <Typography variant="display2" gutterBottom>
+        Lorem Ipsum is
+      </Typography>
+      <p>
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
+        Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer
+        took a galley of type and scrambled it to make a type specimen book. It has survived not only
+        five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+        It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
+        and more recently with desktop publishing software like Aldus PageMaker including
+        versions of Lorem Ipsum.
+      </p>
+    </Features.FeatureInfo>
+    <Features.FeatureInfo>
+      <Typography variant="display2" gutterBottom>
+        Lorem Ipsum is
+      </Typography>
+      <p>
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
+        Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer
+        took a galley of type and scrambled it to make a type specimen book. It has survived not only
+        five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+        It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
+        and more recently with desktop publishing software like Aldus PageMaker including
+        versions of Lorem Ipsum.
+      </p>
+    </Features.FeatureInfo>
+    <Features.FeatureInfo>
+      <Typography variant="display2" gutterBottom>
+        Lorem Ipsum is
+      </Typography>
+      <p>
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
+        Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer
+        took a galley of type and scrambled it to make a type specimen book. It has survived not only
+        five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+        It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
+        and more recently with desktop publishing software like Aldus PageMaker including
+        versions of Lorem Ipsum.
+      </p>
+    </Features.FeatureInfo>
+  </Features.FeaturesWrapper>
+);
+
+Features.FeaturesWrapper = styled.div`
+  && {
+    display        : flex;
+    padding        : 4%;
+    background     : #eaedf5;
+    flex-direction : column;
+
+    ${breakpoint('md')`
+      padding               : 4% 8%;
+      display               : grid;
+      grid-template-columns : 30% 30% 30%;
+      justify-content       : space-between;
+    `}
+  }
+`;
+
+Features.FeatureInfo = styled.div`
+  font-family : 'Roboto', sans-serif;
+  color       : #374142;
+`;
+
+
+export default Features;
---
+++ src/modules/musician/pages/information.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Features from '../components/features';
+
+const InfoForMusicians = () => (
+  <Features />
+);
+
+export default InfoForMusicians;
