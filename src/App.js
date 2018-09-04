fd0c3073fa2d44ec6f4f51c8d8d53ed9f5444668
alextargonski <targonsky97@gmail.com>
2018-09-04 21:32:12 +0300

    add apollo

--- src/App.js
+++ src/App.js
@@ -4,18 +4,32 @@ import { injectGlobal }     from 'styled-components';
 import { ThemeProvider }    from 'styled-components';
 import theme, { muiTheme }  from './layouts/theme';
 import { MuiThemeProvider } from '@material-ui/core/styles';
+import {
+  ApolloClient,
+  createNetworkInterface,
+  ApolloProvider
+}                           from 'react-apollo';
 
+const networkInterface = createNetworkInterface({
+  uri: 'http://localhost:8080/graphql',
+});
+
+const client = new ApolloClient({
+  networkInterface,
+});
 
 class App extends Component {
   render() {
     return (
-      <ThemeProvider theme={theme}>
-        <MuiThemeProvider theme={muiTheme}>
-          <div>
-            {routes}
-          </div>
-        </MuiThemeProvider>
-      </ThemeProvider>
+      <ApolloProvider client={client}>
+        <ThemeProvider theme={theme}>
+          <MuiThemeProvider theme={muiTheme}>
+            <div>
+              {routes}
+            </div>
+          </MuiThemeProvider>
+        </ThemeProvider>
+      </ApolloProvider>
     );
   }
 }
