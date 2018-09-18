c62544d73a3d93baac62fa734f86fc36955cf27b
Oleksandr <targonsky97@gmail.com>
2018-09-18 20:42:33 +0300

    add decode jwt tokens

--- src/routes/private_route.js
+++ src/routes/private_route.js
@@ -1,24 +1,37 @@
-import React from 'react';
+import React  from 'react';
 import {
   Redirect,
   Route,
-}            from 'react-router-dom';
+}             from 'react-router-dom';
+import decode from 'jwt-decode';
 
-const token = localStorage.getItem('token')
+const isAuthenticated = () => {
+  const token = localStorage.getItem('token');
+  const refreshToken = localStorage.getItem('refreshToken');
+  try {
+    decode(token);
+    decode(refreshToken);
+  } catch (err) {
+    return false;
+  }
+
+  return true;
+};
 
 const PrivateRoute = ({ component: Component, ...rest }) => (
-  <Route {...rest} render={props => (
-    token ?
-      <Component {...props} />
-      :
-      <Redirect
-        to={{
-          pathname : '/login',
-          state    : { from: props.location },
-        }}
-      />
-    )}
-  />
-);
+  <Route {...rest}
+    render={props => (
+      isAuthenticated ?
+        <Component {...props} />
+        :
+        <Redirect
+          to={{
+            pathname : '/login',
+            state    : { from: props.location },
+          }}
+        />
+      )}
+    />
+  );
 
 export default PrivateRoute;
