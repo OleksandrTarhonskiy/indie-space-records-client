9b601db670c8ca24e39bed0e5f31e52d507f51ef
Oleksandr <targonsky97@gmail.com>
2018-10-16 10:42:44 +0300

    feat: create edit event form

---
+++ src/modules/events/forms/edit_event.js
@@ -0,0 +1,169 @@
+import React                   from 'react';
+import PropTypes               from 'prop-types';
+import TextField               from '@material-ui/core/TextField';
+import styled                  from 'styled-components';
+import * as R                  from 'ramda';
+import moment                  from 'moment';
+import {
+  CountryDropdown,
+  RegionDropdown,
+}                              from 'react-country-region-selector';
+import {
+  compose,
+  withStateHandlers,
+}                              from 'recompose';
+import { DateTimePicker }      from 'material-ui-pickers';
+import DateFnsUtils            from 'material-ui-pickers/utils/date-fns-utils';
+import MuiPickersUtilsProvider from 'material-ui-pickers/utils/MuiPickersUtilsProvider';
+
+import GradientButton          from '../../../layouts/gradient_button';
+
+const EditEvent = ({
+  form: {
+    title,
+    details,
+    price,
+    date,
+    country,
+    region,
+    address,
+  },
+  handleChange,
+  handleFieldChange,
+}) => (
+  <div>
+    <EditEvent.Headline>
+      Edit event
+    </EditEvent.Headline>
+    <form>
+      <EditEvent.InputsWrapper>
+        <TextField
+          name="title"
+          label="Title"
+          margin="normal"
+          value={title}
+          onChange={handleChange}
+          fullWidth
+        />
+        <TextField
+          name="details"
+          label="details"
+          margin="normal"
+          value={details}
+          onChange={handleChange}
+          fullWidth
+        />
+        <TextField
+          label="Price"
+          value={price}
+          name="price"
+          onChange={handleChange}
+          type="number"
+          InputLabelProps={{
+            shrink: true,
+          }}
+          margin="normal"
+        />
+        <MuiPickersUtilsProvider utils={DateFnsUtils} moment={moment}>
+          <DateTimePicker
+            keyboard
+            label="Started at"
+            minDate={Date.now()}
+            value={date}
+            format="yyyy-MM-dd hh:mm A"
+            disableOpenOnEnter
+            mask={[/\d/, /\d/, /\d/, /\d/, '/', /\d/, /\d/, '/', /\d/, /\d/, ' ', /\d/, /\d/, ':', /\d/, /\d/, ' ', /a|p/i, 'M']}
+            onChange={handleFieldChange.bind(null, 'date')}
+          />
+        </MuiPickersUtilsProvider>
+        <EditEvent.CountryDropdown
+          value={country}
+          onChange={handleFieldChange.bind(null, 'country')}
+        />
+        <EditEvent.RegionDropdown
+          country={country}
+          value={region}
+          onChange={handleFieldChange.bind(null, 'region')}
+        />
+        <TextField
+          name="address"
+          label="address"
+          margin="normal"
+          value={address}
+          onChange={handleChange}
+          fullWidth
+        />
+      </EditEvent.InputsWrapper>
+      <GradientButton
+        text={'Update'}
+      />
+    </form>
+  </div>
+);
+
+EditEvent.InputsWrapper = styled.div`
+  display        : flex;
+  flex-direction : column;
+`;
+
+EditEvent.Headline = styled.h1`
+  font-family : 'Roboto', sans-serif;
+  color       : #374142;
+  text-align  : center;
+  font-weight : 300;
+`;
+
+EditEvent.CountryDropdown = styled(CountryDropdown)`
+  background    : #ffff;
+  border        : 1px solid #999;
+  height        : 33px;
+  border-radius : 0;
+  outline       : none;
+  margin-top    : 2%;
+`;
+
+EditEvent.RegionDropdown = styled(RegionDropdown)`
+  background    : #ffff;
+  border        : 1px solid #999;
+  height        : 33px;
+  border-radius : 0;
+  outline       : none;
+  margin-top    : 2%;
+`;
+
+EditEvent.propTypes = {
+  form              : PropTypes.object.isRequired,
+  canSubmit         : PropTypes.bool.isRequired,
+  handleChange      : PropTypes.func.isRequired,
+  handleFieldChange : PropTypes.func.isRequired,
+};
+
+const withRecompose = compose(
+  withStateHandlers(
+    ({
+      form      = {
+        title   : '',
+        details : '',
+        price   : '',
+        date    : (new Date()).toString(),
+        country : '',
+        region  : '',
+        address : '',
+      },
+      canSubmit = false,
+    }) => ({ form, canSubmit }),
+    {
+      handleChange      : state => ({ target }) => {
+        const form = R.assoc(target.name, target.value, state.form);
+        return ({ form });
+      },
+
+      handleFieldChange : state => (field, value) => {
+        const form = R.assoc(field, value, state.form);
+        return ({ form });
+      },
+    },
+  ),
+);
+
+export default withRecompose(EditEvent);
--- src/modules/events/pages/edit_event_page.js
+++ src/modules/events/pages/edit_event_page.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import React     from 'react';
+import styled    from 'styled-components';
+
+import EditEvent from '../forms/edit_event';
 
 const EditEventPage = () => (
-  <h1>Edit form</h1>
+  <EditEventPage.PageWrapper >
+    <EditEvent />
+  </EditEventPage.PageWrapper >
 );
+
+EditEventPage.PageWrapper = styled.div`
+  padding     : 5% 10%;
+  dispay      : flex;
+  font-family : 'Roboto', sans-serif;
+  color       : #3c3c3e;
+`;
 
 export default EditEventPage;
