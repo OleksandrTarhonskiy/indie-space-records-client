567d80cd1dad24c7685531041e6039a60b197232
Oleksandr <targonsky97@gmail.com>
2018-10-22 15:45:21 +0300

    feat: create profile edit page

---
+++ src/modules/musician/pages/edit_profile_page.js
@@ -0,0 +1,36 @@
+import React            from 'react';
+import { gql, graphql } from 'react-apollo';
+import * as R           from 'ramda';
+import styled           from 'styled-components';
+
+import EditProfileForm  from '../forms/edit_profile_form'
+
+const EditProfilePage = ({ data: { allProfiles = []} }) => (
+  <EditProfilePage.FormWrapper>
+    {
+      allProfiles.map(profile =>
+        <EditProfileForm
+          form={R.assoc('genres', profile.genres.split(","), profile)}
+        />
+      )
+    }
+  </EditProfilePage.FormWrapper>
+);
+
+EditProfilePage.FormWrapper = styled.div`
+  padding : 5% 10%;
+`;
+
+const allProfilesQuery = gql`
+  {
+    allProfiles{
+      id
+      name
+      genres
+      country
+      region
+    }
+  }
+`;
+
+export default graphql(allProfilesQuery)(EditProfilePage);
