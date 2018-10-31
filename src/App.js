51fa19698b4da45fd92b689af766863a951b710a
Oleksandr <targonsky97@gmail.com>
2018-10-31 13:05:05 +0200

    refactor: put ApolloClient into separate file

--- src/App.js
+++ src/App.js
@@ -4,50 +4,11 @@ import { injectGlobal }     from 'styled-components';
 import { ThemeProvider }    from 'styled-components';
 import theme, { muiTheme }  from './layouts/theme';
 import { MuiThemeProvider } from '@material-ui/core/styles';
-import { ApolloClient }     from 'apollo-client';
-import { createHttpLink }   from 'apollo-link-http';
-import { InMemoryCache }    from 'apollo-cache-inmemory';
 import { ApolloProvider }   from 'react-apollo';
-import { setContext }       from 'apollo-link-context';
-import { ApolloLink }       from 'apollo-link';
 
 import Routes                from './routes';
 import registerServiceWorker from './registerServiceWorker';
-
-const httpLink = createHttpLink({ uri: 'http://localhost:8080/graphql' });
-
-const middlewareLink = setContext(() => ({
-  headers: {
-    'x-token': localStorage.getItem('token'),
-    'x-refresh-token': localStorage.getItem('refreshToken'),
-  },
-}));
-
-const afterwareLink = new ApolloLink((operation, forward) => {
-  const { headers } = operation.getContext();
-
-  if (headers) {
-    const token = headers.get('x-token');
-    const refreshToken = headers.get('x-refresh-token');
-
-    if (token) {
-      localStorage.setItem('token', token);
-    }
-
-    if (refreshToken) {
-      localStorage.setItem('refreshToken', refreshToken);
-    }
-  }
-
-  return forward(operation);
-});
-
-const link = afterwareLink.concat(middlewareLink.concat(httpLink));
-
-const client = new ApolloClient({
-  link,
-  cache: new InMemoryCache(),
-});
+import client                from './graphql/client'
 
 class App extends Component {
   render() {
