e4d2de72f6f37f8f7704fd41e804720c70be4144
Oleksandr <targonsky97@gmail.com>
2018-11-06 14:01:58 +0200

    feat: add routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -25,6 +25,7 @@ import EventPage                from '../modules/events/pages/event_page';
 import AllThemesPage            from '../modules/themes/pages/all_themes_page';
 import FlatTheme                from '../modules/themes/components/flat_theme';
 import BeautifulPlay            from '../modules/themes/components/beautiful_play';
+import SongUpload               from '../modules/songs/pages/song_upload'
 import PrivateRoute             from './private_route';
 
 export default (
@@ -49,6 +50,7 @@ export default (
         <PrivateRoute exact path="/profile/settings" component={ProfileThemeSettingsPage} />
         <PrivateRoute exact path="/profile/events" component={MusicianEventsPage} />
         <PrivateRoute path="/profile" component={SettingsPage} />
+        <PrivateRoute path="/upload_song" component={SongUpload} />
         <Route component={NotFound} />
       </Switch>
       <Footer />
