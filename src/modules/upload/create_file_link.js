51b2e9eacc138ce79e02e354fed2509f2307e620
Oleksandr <targonsky97@gmail.com>
2018-11-08 16:14:29 +0200

    bug: update create_file_link

--- src/modules/upload/create_file_link.js
+++ src/modules/upload/create_file_link.js
@@ -84,9 +84,8 @@ export default ({ uri, includeExtensions, ...requestOptions } = {}) => {
       };
 
       if (has(variables, 'file')) {
-        const stringBody = serializedBody;
         serializedBody = new FormData();
-        serializedBody.append('operations', stringBody);
+        serializedBody.append('operations', JSON.stringify(body));
         serializedBody.append('file', variables.file);
       } else {
         myHeaders['content-type'] = 'application/json';
