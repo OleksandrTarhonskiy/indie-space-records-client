844522e0daae0c1c0fab84c0412ffb7bd90b832c
Oleksandr <targonsky97@gmail.com>
2018-11-16 16:08:20 +0200

    feat: add product inline edit handler

--- src/modules/merch/components/product_row.js
+++ src/modules/merch/components/product_row.js
@@ -1,7 +1,15 @@
-import React     from 'react';
-import PropTypes from 'prop-types';
-import TableCell from '@material-ui/core/TableCell';
-import TableRow  from '@material-ui/core/TableRow';
+import React         from 'react';
+import PropTypes     from 'prop-types';
+import TableCell     from '@material-ui/core/TableCell';
+import TableRow      from '@material-ui/core/TableRow';
+import TextField     from '@material-ui/core/TextField';
+import IconButton    from '@material-ui/core/IconButton';
+import CloseIcon     from '@material-ui/icons/Close';
+import * as R        from 'ramda';
+import {
+  compose,
+  withStateHandlers,
+}                    from 'recompose';
 
 const ProductRow = ({
   product: {
@@ -9,13 +17,79 @@ const ProductRow = ({
     title,
     price,
     inStock,
-  }
+  },
+  toggleEdit,
+  edit,
 }) => (
   <TableRow>
-    <TableCell>{type}</TableCell>
-    <TableCell>{title}</TableCell>
-    <TableCell>{price}</TableCell>
-    <TableCell>{String(inStock)}</TableCell>
+    {
+      edit.type ?
+      <TableCell>
+        <TextField
+          name="type"
+          label="type"
+          margin="normal"
+          value={type}
+          fullWidth
+        />
+        <IconButton onClick={toggleEdit.bind(null, 'type', false)}>
+          <CloseIcon />
+        </IconButton>
+      </TableCell>
+      :
+      <TableCell onClick={toggleEdit.bind(null, 'type', true)}>{type}</TableCell>
+    }
+    {
+      edit.title ?
+      <TableCell>
+        <TextField
+          name="title"
+          label="Title"
+          margin="normal"
+          value={title}
+          fullWidth
+        />
+        <IconButton onClick={toggleEdit.bind(null, 'title', false)}>
+          <CloseIcon />
+        </IconButton>
+      </TableCell>
+      :
+      <TableCell onClick={toggleEdit.bind(null, 'title', true)}>{title}</TableCell>
+    }
+    {
+      edit.price ?
+      <TableCell>
+        <TextField
+          name="price"
+          label="price"
+          margin="normal"
+          value={price}
+          fullWidth
+        />
+        <IconButton onClick={toggleEdit.bind(null, 'price', false)}>
+          <CloseIcon />
+        </IconButton>
+      </TableCell>
+      :
+      <TableCell onClick={toggleEdit.bind(null, 'price', true)}>{price}</TableCell>
+    }
+    {
+      edit.inStock ?
+      <TableCell>
+        <TextField
+          name="inStock"
+          label="in stock"
+          margin="normal"
+          value={inStock}
+          fullWidth
+        />
+        <IconButton onClick={toggleEdit.bind(null, 'inStock', false)}>
+          <CloseIcon />
+        </IconButton>
+      </TableCell>
+      :
+      <TableCell onClick={toggleEdit.bind(null, 'inStock', true)}>{String(inStock)}</TableCell>
+    }
     <TableCell numeric>{0}</TableCell>
     <TableCell numeric>{0}</TableCell>
   </TableRow>
@@ -25,4 +99,23 @@ ProductRow.propTypes = {
   product : PropTypes.object.isRequired,
 };
 
-export default ProductRow;
+const withRecompose = compose(
+  withStateHandlers(
+    ({
+      edit = {
+        type : false,
+        title : false,
+        price : false,
+        inStock : false,
+      },
+    }) => ({ edit }),
+    {
+      toggleEdit : state => (cell, value) => {
+        const edit = R.assoc(cell, value, state.edit);
+        return ({ edit });
+      },
+    },
+  ),
+);
+
+export default withRecompose(ProductRow);
