570ac1fdc6f33159a07b19236162df0159c761c3
Oleksandr <targonsky97@gmail.com>
2018-11-18 13:01:04 +0200

    feat: add handlers for some inputs

--- src/modules/merch/components/product_row.js
+++ src/modules/merch/components/product_row.js
@@ -1,16 +1,23 @@
-import React         from 'react';
-import PropTypes     from 'prop-types';
-import styled        from 'styled-components';
-import TableCell     from '@material-ui/core/TableCell';
-import TableRow      from '@material-ui/core/TableRow';
-import TextField     from '@material-ui/core/TextField';
-import IconButton    from '@material-ui/core/IconButton';
-import CloseIcon     from '@material-ui/icons/Close';
-import * as R        from 'ramda';
+import React              from 'react';
+import PropTypes          from 'prop-types';
+import styled             from 'styled-components';
+import TableCell          from '@material-ui/core/TableCell';
+import TableRow           from '@material-ui/core/TableRow';
+import TextField          from '@material-ui/core/TextField';
+import InputLabel         from '@material-ui/core/InputLabel';
+import FormControl        from '@material-ui/core/FormControl';
+import Input              from '@material-ui/core/Input';
+import Select             from '@material-ui/core/Select';
+import MenuItem           from '@material-ui/core/MenuItem';
+import IconButton         from '@material-ui/core/IconButton';
+import CloseIcon          from '@material-ui/icons/Close';
+import * as R             from 'ramda';
 import {
   compose,
   withStateHandlers,
-}                    from 'recompose';
+}                         from 'recompose';
+
+import { PRODUCTS_TYPES } from '../models/types';
 
 const ProductRow = ({
   product: {
@@ -21,18 +28,34 @@ const ProductRow = ({
   },
   toggleEdit,
   edit,
+  handleChange,
 }) => (
   <TableRow>
     {
       edit.type ?
       <ProductRow.TableCell>
-        <TextField
-          name="type"
-          label="type"
-          margin="normal"
-          value={type}
-          fullWidth
-        />
+        <ProductRow.SelectWrapper>
+          <InputLabel
+            ref={ref => {
+              this.InputLabelRef = ref;
+            }}
+            htmlFor="type"
+          >
+            Type of product
+          </InputLabel>
+          <Select
+            value={type}
+            onChange={handleChange}
+            input={
+              <Input
+                name="type"
+                id="type"
+              />
+            }
+          >
+            { PRODUCTS_TYPES.map((t, index) => <MenuItem key={index} value={t}>{t}</MenuItem>) }
+          </Select>
+        </ProductRow.SelectWrapper>
         <ProductRow.IconButton onClick={toggleEdit.bind(null, 'type', false)}>
           <CloseIcon />
         </ProductRow.IconButton>
@@ -49,6 +72,7 @@ const ProductRow = ({
           name="title"
           label="Title"
           margin="normal"
+          onChange={handleChange}
           value={title}
           fullWidth
         />
@@ -65,11 +89,15 @@ const ProductRow = ({
       edit.price ?
       <ProductRow.TableCell>
         <TextField
-          name="price"
-          label="price"
-          margin="normal"
+          label="Price"
           value={price}
-          fullWidth
+          onChange={handleChange}
+          name="price"
+          type="number"
+          InputLabelProps={{
+            shrink: true,
+          }}
+          margin="normal"
         />
         <ProductRow.IconButton onClick={toggleEdit.bind(null, 'price', false)}>
           <CloseIcon />
@@ -117,6 +145,11 @@ ProductRow.IconButton = styled(IconButton)`
   margin : 25px !important;
 `;
 
+ProductRow.SelectWrapper = styled(FormControl)`
+  width       : 70%;
+  padding-top : 14px !important;
+`;
+
 const withRecompose = compose(
   withStateHandlers(
     ({
@@ -126,11 +159,21 @@ const withRecompose = compose(
         price   : false,
         inStock : false,
       },
-    }) => ({ edit }),
+      product = {
+        type         : '',
+        title        : '',
+        price        : 0,
+      },
+    }) => ({ edit, product }),
     {
       toggleEdit : state => (cell, value) => {
         const edit = R.assoc(cell, value, state.edit);
         return ({ edit });
+      },
+
+      handleChange : state => ({ target }) => {
+        const product = R.assoc(target.name, target.value, state.product);
+        return ({ product });
       },
     },
   ),
