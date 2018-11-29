0fbe9edd6e3689c06d441589dcf48dfe643f4a71
Oleksandr <targonsky97@gmail.com>
2018-11-29 18:23:29 +0200

    feat: change query

--- src/modules/musician/graphql/queries.js
+++ src/modules/musician/graphql/queries.js
@@ -25,6 +25,12 @@ export const myProfileWithThemeQuery = gql`
       theme {
         style
         fonts
+        sections {
+          name
+          type
+          content
+          style
+        }
       }
     }
   }
