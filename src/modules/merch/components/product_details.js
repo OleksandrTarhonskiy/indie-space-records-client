44cde3548258bfce51c6fa2803f9149400325c67
Oleksandr <targonsky97@gmail.com>
2019-01-25 16:42:31 +0200

    style: use section background-color on product page

--- src/modules/merch/components/product_details.js
+++ src/modules/merch/components/product_details.js
@@ -1,15 +1,15 @@
-import React                from 'react';
-import PropTypes            from 'prop-types';
-import styled               from 'styled-components';
-import breakpoint           from 'styled-components-breakpoint';
-import { graphql }          from 'react-apollo';
-import Button               from '@material-ui/core/Button';
-import ShoppingCart         from '@material-ui/icons/ShoppingCart';
-import { compose }          from 'recompose';
-import { withRouter }       from 'react-router';
-import CircularProgress     from '@material-ui/core/CircularProgress';
+import React                                 from 'react';
+import PropTypes                             from 'prop-types';
+import styled                                from 'styled-components';
+import breakpoint                            from 'styled-components-breakpoint';
+import { graphql }                           from 'react-apollo';
+import Button                                from '@material-ui/core/Button';
+import ShoppingCart                          from '@material-ui/icons/ShoppingCart';
+import { compose }                           from 'recompose';
+import { withRouter }                        from 'react-router';
+import CircularProgress                      from '@material-ui/core/CircularProgress';
 
-import { profileThemeQuery } from '../../musician/graphql/queries';
+import { fetchProfileProductsAndThemeQuery } from '../../musician/graphql/queries';
 
 const ProductDetails = ({
   product,
@@ -18,42 +18,48 @@ const ProductDetails = ({
     loading,
   },
 }) => (
-  <ProductDetails.Wrapper>
-    <ProductDetails.ImageWrapper>
-      <ProductDetails.Image
-        src={`http://localhost:8080/${product.url}`}
-        alt=""
-      />
-    </ProductDetails.ImageWrapper>
-    <ProductDetails.DetailsBlock>
-      <h1>{product.title}</h1>
-      {
-        loading ?
-          <CircularProgress />
-          :
-          <React.Fragment>
-            <h2>{product.price} {fetchProfile.currency}</h2>
-            <h3>{product.type}</h3>
-            <p>{product.desc}</p>
-            <ProductDetails.AddToCart
-              basicStyles={JSON.parse(fetchProfile.theme.style)}
-              disabled={!product.quantity}
-            >
-              <ShoppingCart />
-            Add to cart
-            </ProductDetails.AddToCart>
-          </React.Fragment>
-      }
-    </ProductDetails.DetailsBlock>
-  </ProductDetails.Wrapper>
+  <React.Fragment>
+    {
+      loading ?
+      <CircularProgress />
+      :
+      <ProductDetails.Wrapper
+        sectionStyles={JSON.parse(fetchProfile.theme.sections.find((element) => element.type === 'merch').style)}
+      >
+        <ProductDetails.ImageWrapper>
+          <ProductDetails.Image
+            src={`http://localhost:8080/${product.url}`}
+            alt=""
+          />
+        </ProductDetails.ImageWrapper>
+        <ProductDetails.DetailsBlock>
+          <h1>{product.title}</h1>
+              <React.Fragment>
+                <h2>{product.price} {fetchProfile.currency}</h2>
+                <h3>{product.type}</h3>
+                <p>{product.desc}</p>
+                <ProductDetails.AddToCart
+                  basicStyles={JSON.parse(fetchProfile.theme.style)}
+                  disabled={!product.quantity}
+                >
+                  <ShoppingCart />
+                Add to cart
+                </ProductDetails.AddToCart>
+              </React.Fragment>
+
+        </ProductDetails.DetailsBlock>
+      </ProductDetails.Wrapper>
+    }
+  </React.Fragment>
 );
 
 ProductDetails.Wrapper = styled.div`
   && {
-    display        : flex;
-    flex-direction : column;
-    font-family    : 'Roboto', sans-serif;
-    color          : #3c3c3e;
+    background-color : ${props => props.sectionStyles.background};
+    display          : flex;
+    flex-direction   : column;
+    font-family      : 'Roboto', sans-serif;
+    color            : ${props => props.sectionStyles.color};
 
     ${breakpoint('md')`
       flex-direction : row;
@@ -82,7 +88,7 @@ ProductDetails.AddToCart = styled(Button)`
   && {
     width         : 300px;
     border        : ${props => props.basicStyles.border}px solid;;
-    background    : ${ props => props.disabled ? '#ffff' : props.basicStyles.buttonsBackground};
+    background    : ${props => props.disabled ? '#ffff' : props.basicStyles.buttonsBackground};
     color         : ${props => props.basicStyles.buttonsColor};
     padding       : 15px 30px;
     border-radius : ${props => props.basicStyles.borderRadius}px;
@@ -103,7 +109,7 @@ ProductDetails.propTypes = {
 
 const withRecompose = compose(
   withRouter,
-  graphql(profileThemeQuery, {
+  graphql(fetchProfileProductsAndThemeQuery, {
     options: (props) => ({
       variables: {
         profileId: props.match.params.musicianId,
