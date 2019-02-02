9d08a24bfd754e346fd32551c49873ea0c863ef8
Oleksandr <targonsky97@gmail.com>
2019-02-02 14:21:36 +0200

    feat: add infinite-scroll to full events page

--- src/modules/musician/components/full_events_list.js
+++ src/modules/musician/components/full_events_list.js
@@ -4,48 +4,79 @@ import styled     from 'styled-components';
 import breakpoint from 'styled-components-breakpoint';
 import moment     from 'moment';
 import Button     from '@material-ui/core/Button';
+import { graphql }            from 'react-apollo';
+import {
+  compose,
+  withHandlers,
+  withState,
+}                             from 'recompose';
+import InfiniteScroll         from 'react-infinite-scroller';
+import CircularProgress       from '@material-ui/core/CircularProgress';
 
-const FullEventsList = ({ profile }) => (
+import { viewEventsQuery } from '../../events/graphql/queries';
+
+const FullEventsList = ({
+  profile,
+  data: {
+    loading,
+    events = [],
+  },
+  loadMore,
+  hasMore,
+}) => (
   <FullEventsList.PageWrapper
     profileFonts={JSON.parse(profile.theme.fonts)}
     profileStyles={JSON.parse(profile.theme.style)}
     sectionStyles={JSON.parse(profile.theme.sections.find((element) => element.type === 'events').style)}
   >
     {
-      profile.events.map(e =>
-        <FullEventsList.EventsItem key={e.id}>
-          <FullEventsList.Cell
-            elementFont={JSON.parse(profile.theme.fonts)}
-            elementStyles={JSON.parse(profile.theme.style)}
-          >
-            {moment(e.date).format('D MMM HH:mm')}
-          </FullEventsList.Cell>
-          <FullEventsList.Cell
-            elementFont={JSON.parse(profile.theme.fonts)}
-            elementStyles={JSON.parse(profile.theme.style)}
-          >
-            {e.title}
-          </FullEventsList.Cell>
-          <FullEventsList.Cell
-            elementFont={JSON.parse(profile.theme.fonts)}
-            elementStyles={JSON.parse(profile.theme.style)}
-          >
-            {e.address}
-          </FullEventsList.Cell>
-          <FullEventsList.Cell
-            elementFont={JSON.parse(profile.theme.fonts)}
-            elementStyles={JSON.parse(profile.theme.style)}
-          >
-            {e.price}
-          </FullEventsList.Cell>
-          <FullEventsList.Button
-            elementFont={JSON.parse(profile.theme.fonts)}
-            elementStyles={JSON.parse(profile.theme.style)}
-          >
-          Tikets
-          </FullEventsList.Button>
-        </FullEventsList.EventsItem>
-      )
+      loading ?
+        <CircularProgress />
+        :
+        <InfiniteScroll
+          pageStart={0}
+          loadMore={setTimeout(loadMore, 2000)}
+          hasMore={hasMore}
+          loader={<CircularProgress key={0} />}
+          useWindow={true}
+        >
+        {
+          events.map(e =>
+            <FullEventsList.EventsItem key={e.id}>
+              <FullEventsList.Cell
+                elementFont={JSON.parse(profile.theme.fonts)}
+                elementStyles={JSON.parse(profile.theme.style)}
+              >
+                {moment(e.date).format('D MMM HH:mm')}
+              </FullEventsList.Cell>
+              <FullEventsList.Cell
+                elementFont={JSON.parse(profile.theme.fonts)}
+                elementStyles={JSON.parse(profile.theme.style)}
+              >
+                {e.title}
+              </FullEventsList.Cell>
+              <FullEventsList.Cell
+                elementFont={JSON.parse(profile.theme.fonts)}
+                elementStyles={JSON.parse(profile.theme.style)}
+              >
+                {e.address}
+              </FullEventsList.Cell>
+              <FullEventsList.Cell
+                elementFont={JSON.parse(profile.theme.fonts)}
+                elementStyles={JSON.parse(profile.theme.style)}
+              >
+                {e.price}
+              </FullEventsList.Cell>
+              <FullEventsList.Button
+                elementFont={JSON.parse(profile.theme.fonts)}
+                elementStyles={JSON.parse(profile.theme.style)}
+              >
+              Tikets
+              </FullEventsList.Button>
+            </FullEventsList.EventsItem>
+          )
+        }
+      </InfiniteScroll>
     }
   </FullEventsList.PageWrapper>
 );
@@ -101,4 +132,43 @@ FullEventsList.propTypes = {
   profile : PropTypes.object.isRequired,
 };
 
-export default FullEventsList;
+const withRecompose = compose(
+  graphql(viewEventsQuery, {
+    options: props => ({
+      fetchPolicy: 'network-only',
+      variables: {
+        profileId : props.profile.id,
+        offset    : 0,
+      },
+    }),
+  }),
+  withState('hasMore', 'setHasMore', true),
+  withHandlers(
+    {
+      loadMore : ({ data, setHasMore }) => () => {
+        data.fetchMore({
+          variables : {
+            offset : data.events.length,
+          },
+
+          updateQuery : (previousResult, { fetchMoreResult }) => {
+            if (!fetchMoreResult) {
+              return previousResult;
+            }
+
+            if (fetchMoreResult.events.length < 5) {
+              setHasMore(false);
+            }
+
+            return {
+              ...previousResult,
+              events: [...previousResult.events, ...fetchMoreResult.events],
+            };
+          },
+        });
+      }
+    },
+  ),
+);
+
+export default withRecompose(FullEventsList);
