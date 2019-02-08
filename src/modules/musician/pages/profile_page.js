522b897ae2af53149e73edb097166dd0a70563e5
Oleksandr <targonsky97@gmail.com>
2019-02-08 15:58:46 +0200

    refactor: remove profile component

--- src/modules/musician/pages/profile_page.js
+++ src/modules/musician/pages/profile_page.js
@@ -4,25 +4,89 @@ import { withRouter }        from 'react-router';
 import { graphql }           from 'react-apollo';
 import { compose }           from 'recompose';
 import CircularProgress      from '@material-ui/core/CircularProgress';
+import styled                from 'styled-components';
+import { Helmet }            from 'react-helmet';
 
 import { fetchProfileQuery } from '../graphql/queries';
-import Profile               from '../components/profile';
+import Sections              from '../components/sections';
 
 const ProfilePage = ({
   data: {
     loading,
     fetchProfile = {}
   },
+  children,
 }) => (
   <React.Fragment>
     {
       loading ?
         <CircularProgress />
         :
-        <Profile profile={fetchProfile} />
+        <ProfilePage.Body
+          elementStyles={JSON.parse(fetchProfile.theme.style)}
+        >
+          <Helmet>
+            <link href={`https://fonts.googleapis.com/css?family=${JSON.parse(fetchProfile.theme.fonts).headlineFont}`} rel="stylesheet" />
+            <link href={`https://fonts.googleapis.com/css?family=${JSON.parse(fetchProfile.theme.fonts).regularTextFont}`} rel="stylesheet" />
+            <link href={`https://fonts.googleapis.com/css?family=${JSON.parse(fetchProfile.theme.fonts).linksFont}`} rel="stylesheet" />
+            <link href={`https://fonts.googleapis.com/css?family=${JSON.parse(fetchProfile.theme.fonts).subHead}`} rel="stylesheet" />
+          </Helmet>
+          <ProfilePage.Header elementStyles={JSON.parse(fetchProfile.theme.style)}>
+            <ProfilePage.NavItems>
+              {
+                fetchProfile.theme.sections.map(section =>
+                  <ProfilePage.NavItem key={section.id}>
+                    <ProfilePage.Link
+                      href=""
+                      elementStyles={JSON.parse(fetchProfile.theme.style)}
+                      elementFont={JSON.parse(fetchProfile.theme.fonts)}
+                    >
+                      {section.name}
+                    </ProfilePage.Link>
+                  </ProfilePage.NavItem>
+                )
+              }
+            </ProfilePage.NavItems>
+          </ProfilePage.Header>
+          <Sections profile={fetchProfile} />
+        </ProfilePage.Body>
     }
   </React.Fragment>
 );
+
+ProfilePage.Body = styled.div`
+  background-color : ${props => props.elementStyles.backgroundColor};
+  position         : relative;
+`;
+
+ProfilePage.Header = styled.div`
+  background-color : ${props => props.elementStyles.headerBackground};
+  text-align       : ${props => props.elementStyles.MenuLinksPosition};
+`;
+
+ProfilePage.NavItems = styled.ul`
+  margin  : 0;
+  padding : 0;
+`;
+
+ProfilePage.Link = styled.a`
+  && {
+    color           : ${props => props.elementStyles.LinksColor};
+    text-decoration : none;
+    font-family     : ${props => `${props.elementFont.linksFont}`}, sans-serif;
+    font-weight     : 600;
+    outline         : none;
+
+    &:hover {
+      color : ${props => props.elementStyles.LinksHover};
+    }
+  }
+`;
+
+ProfilePage.NavItem = styled.li`
+  display : inline-block;
+  padding : 2%;
+`;
 
 ProfilePage.propTypes = {
   match : PropTypes.object.isRequired,
