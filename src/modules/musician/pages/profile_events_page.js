ab689e4b8bcb51b4dc0f1b1a89a88bf866b5802b
Oleksandr <targonsky97@gmail.com>
2019-02-11 15:51:30 +0200

    refactor: remove old events page (wrapper)

--- src/modules/musician/pages/profile_events_page.js
+++
@@ -1,9 +0,0 @@
-import React             from 'react';
-
-import FullEventsList    from '../components/full_events_list';
-
-const ProfileEventsPage = () => (
-  <FullEventsList />
-);
-
-export default ProfileEventsPage;
