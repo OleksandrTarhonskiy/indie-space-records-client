d692af4a58c829a188be98030d9bc3192b0e3e4d
Oleksandr <targonsky97@gmail.com>
2019-02-11 16:01:44 +0200

    refactor: remove old merch page (wrapper)

--- src/modules/musician/pages/musician_merch_page.js
+++
@@ -1,9 +0,0 @@
-import React                             from 'react';
-
-import FullMerchList                     from '../components/full_merch_list';
-
-const MusicianMerchPage = () => (
-  <FullMerchList />
-);
-
-export default MusicianMerchPage;
