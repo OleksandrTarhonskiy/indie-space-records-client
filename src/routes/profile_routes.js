2a00151157258f56d8728ac668ba6a8459c1b118
Oleksandr <targonsky97@gmail.com>
2019-02-11 15:46:30 +0200

    fix: change import

--- src/routes/profile_routes.js
+++ src/routes/profile_routes.js
@@ -4,11 +4,11 @@ import { Route }         from 'react-router-dom';
 import MusicianMerchPage from '../modules/musician/pages/musician_merch_page';
 import ProfileEventsPage from '../modules/musician/pages/profile_events_page';
 import ProductPage       from '../modules/merch/pages/product_page';
-import ProfilePage       from '../modules/musician/pages/profile_page';
+import ProfileHomePage   from '../modules/musician/pages/profile_home_page';
 
 export default (
   <React.Fragment>
-    <Route exact path="/musicians/:id"                  component={ProfilePage} />
+    <Route exact path="/musicians/:id"                  component={ProfileHomePage} />
     <Route exact path="/musicians/:id/merch"            component={MusicianMerchPage} />
     <Route exact path="/musicians/:id/events"           component={ProfileEventsPage} />
     <Route exact path="/musicians/:id/merch/:productId" component={ProductPage} />
