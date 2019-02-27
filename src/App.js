9fbddc95f13ffdf37a181745aa227222fe53dc62
Oleksandr <targonsky97@gmail.com>
2019-02-27 17:45:50 +0200

    feat: add Badge

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import theme, { muiTheme }  from './layouts/theme';
 import { MuiThemeProvider } from '@material-ui/core/styles';
 import { ApolloProvider }   from 'react-apollo';
 import ShoppingCart         from '@material-ui/icons/ShoppingCart';
+import Badge                from '@material-ui/core/Badge';
 import IconButton           from '@material-ui/core/IconButton';
 
 import client               from './graphql/client';
@@ -21,17 +22,47 @@ class App extends Component {
             <div>
               {routes}
             </div>
-            <CartButton>
-              <IconButton>
-                <ShoppingCart />
-              </IconButton>
-            </CartButton>
+            {
+              JSON.parse(localStorage.getItem('Cart'))&&
+              <CartButton>
+                <CartButtonBadge badgeContent={JSON.parse(localStorage.getItem('Cart')).length} color="primary">
+                  <ShoppingCartButton>
+                    <ShoppingCart />
+                  </ShoppingCartButton>
+                </CartButtonBadge>
+              </CartButton>
+            }
           </MuiThemeProvider>
         </ThemeProvider>
       </ApolloProvider>
     );
   }
 }
+
+const CartButtonBadge = styled(Badge)`
+  position   : fixed !important;
+  z-index    : 3000;
+  text-align : center;
+  bottom     : 100px;
+  right      : 25px;
+  position   : fixed !important;
+  z-index    : 3000;
+  text-align : center;
+  z-index    : 3500;
+`;
+
+
+const ShoppingCartButton = styled(IconButton)`
+  position   : fixed !important;
+  z-index    : 3000;
+  text-align : center;
+  bottom     : 20px;
+  right      : 20px;
+  background : #ffff !important;
+  height     : 80px;
+  width      : 80px;
+  box-shadow : 3px 10px 5px -8px rgba(0,0,0,0.75);
+`;
 
 injectGlobal`
   body {
