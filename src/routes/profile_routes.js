5a4ba02f0b864b8edf660aeeec7d5462f9596ba2
Oleksandr <targonsky97@gmail.com>
2019-04-22 12:15:49 +0300

    refactor: change checkout form location

--- src/routes/profile_routes.js
+++ src/routes/profile_routes.js
@@ -5,6 +5,7 @@ import AllMerchPage    from '../modules/musician/pages/all_merch_page';
 import AllEventsPage   from '../modules/musician/pages/all_events_page';
 import ProductPage     from '../modules/merch/pages/product_page';
 import ProfileHomePage from '../modules/musician/pages/profile_home_page';
+import OrderForm       from '../modules/merch/forms/order_form';
 
 export default (
   <React.Fragment>
@@ -12,5 +13,6 @@ export default (
     <Route exact path="/musicians/:id/merch"            component={AllMerchPage} />
     <Route exact path="/musicians/:id/events"           component={AllEventsPage} />
     <Route exact path="/musicians/:id/merch/:productId" component={ProductPage} />
+    <Route exact path="/musicians/:id/checkout"         component={OrderForm} />
   </React.Fragment>
 );
